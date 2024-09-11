004de06b69c18f46cc2ddab1c93757b416191f5e
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2024-09-11 17:43:33 +0500

    Update Sidebar.jsx

--- coldstoragemanagementsystem/src/components/Sidebar.jsx
+++ coldstoragemanagementsystem/src/components/Sidebar.jsx
@@ -5,10 +5,13 @@ import { FaBarsProgress } from "react-icons/fa6";
 import { SlCalender } from "react-icons/sl";
 import { IoIosSettings } from "react-icons/io";
 import { IoLogOutSharp } from "react-icons/io5";
+import { IoBagSharp } from "react-icons/io5";
+import { FaUserEdit } from "react-icons/fa";
+import { FaBox } from "react-icons/fa";
 function Sidebar(){
     return(
         <>
-       <div className="main bg-gray-100 px-5 py-7 h-screen max-w-40">
+       <div className="main bg-gray-100 px-5 py-7 h-screen max-w-48">
  <div className=" h-full flex flex-col justify-between">
 <div>
     <h1  className="font-semibold text-xs">
@@ -16,9 +19,12 @@ function Sidebar(){
     </h1>
     <div className="mt-8">
         <ul className="space-y-2">
-            <li className="px-3 py-2 bg-black text-white rounded-md flex items-center space-x-1"> <MdDashboard className="text-sm"/> <p className="text-xs">Dashboard</p></li>
-            <li className="px-3 py-2   text-black rounded-md flex items-center space-x-1"> <FaBarsProgress className="text-sm"/> <p className="text-xs">Progress</p></li>
-            <li className="px-3 py-2   text-black rounded-md flex items-center space-x-1"> <SlCalender className="text-sm"/> <p className="text-xs">Calender</p></li>
+            <li className="px-3 py-2 bg-black text-white rounded-md flex items-center space-x-1"> <MdDashboard className="text-sm"/>
+             <p className="text-xs">Dashboard</p>
+             </li>
+            <li className="px-3 py-2   text-black rounded-md flex items-center space-x-1"> <IoBagSharp className="text-sm"/> <p className="text-xs">Manage Inventory </p></li>
+            <li className="px-3 py-2   text-black rounded-md flex items-center space-x-1"> <FaUserEdit className="text-sm"/> <p className="text-xs">Manage Supplier</p></li>
+            <li className="px-3 py-2   text-black rounded-md flex items-center space-x-1"> <FaBox className="text-sm"/> <p className="text-xs">Manage Storage</p></li>
             <li className="px-3 py-2   text-black rounded-md flex items-center space-x-1"> <IoIosSettings className="text-sm"/> <p className="text-xs">Settings</p></li>
         </ul>
         <div>

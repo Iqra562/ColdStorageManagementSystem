0bcf32001cdaf8cee19e4e0f9e6342e0882980ce
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2024-09-16 12:19:27 +0500

    label atyling

--- coldstoragemanagementsystem/src/pages/Inventory/AddInventory.jsx
+++ coldstoragemanagementsystem/src/pages/Inventory/AddInventory.jsx
@@ -9,86 +9,86 @@ return(
     <h1 className="text-lg font-semibold">Add Inventory</h1>
   </div>
   <form action="">
-    <div className="flex flex-col space-y-6">
-      
-      <div className="flex items-center space-x-3">
-        <label className="text-sm text-gray-500">Inventory Name</label>
-        <input
-          type="text"
-          placeholder="Inventory Name"
-          className="flex-grow text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
-        />
-      </div>
+  <div className="flex flex-col space-y-6">
+  <div className="flex items-center justify-between">
+    <label className="text-sm text-gray-500">Inventory Name</label>
+    <input
+      type="text"
+      placeholder="Inventory Name"
+      className="w-10/12 text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
+    />
+  </div>
 
-      <div className="flex items-center space-x-3">
-        <label className="text-sm text-gray-500">Inventory Code</label>
-        <input
-          type="number"
-          placeholder="Inventory Code"
-          className="flex-grow text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
-        />
-      </div>
+  <div className="flex items-center justify-between">
+    <label className="text-sm text-gray-500">Inventory Code</label>
+    <input
+      type="number"
+      placeholder="Inventory Code"
+      className="w-10/12 text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
+    />
+  </div>
 
-      <div className="flex items-center space-x-3">
-        <label className="text-sm text-gray-500">Inventory Quantity</label>
-        <input
-          type="text"
-          placeholder="Inventory Quantity"
-          className="flex-grow text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
-        />
-      </div>
+  <div className="flex items-center justify-between">
+    <label className="text-sm text-gray-500">Inventory Quantity</label>
+    <input
+      type="text"
+      placeholder="Inventory Quantity"
+      className="w-10/12 text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
+    />
+  </div>
 
-      <div className="flex items-center space-x-3">
-        <label className="text-sm text-gray-500">Storage Temperature</label>
-        <input
-          type="number"
-          placeholder="Storage Temperature"
-          className="flex-grow text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
-        />
-      </div>
+  <div className="flex items-center justify-between">
+    <label className="text-sm text-gray-500">Storage Temperature</label>
+    <input
+      type="number"
+      placeholder="Storage Temperature"
+      className="w-10/12 text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
+    />
+  </div>
 
-      <div className="flex items-center space-x-3">
-        <label className="text-sm text-gray-500">Storage Section Number</label>
-        <input
-          type="number"
-          placeholder="Storage Section Number"
-          className="flex-grow text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
-        />
-      </div>
+  <div className="flex items-center justify-between">
+    <label className="text-sm text-gray-500">Storage Section Number</label>
+    <input
+      type="number"
+      placeholder="Storage Section Number"
+      className="w-10/12 text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
+    />
+  </div>
 
-      <div className="flex items-center space-x-3">
-        <label className="text-sm text-gray-500">Date of Entry</label>
-        <input
-          type="date"
-          placeholder="Date of Entry"
-          className="flex-grow text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
-        />
-      </div>
+  <div className="flex items-center justify-between">
+    <label className="text-sm text-gray-500">Date of Entry</label>
+    <input
+      type="date"
+      placeholder="Date of Entry"
+      className="w-10/12 text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
+    />
+  </div>
 
-      <div className="flex items-center space-x-3">
-        <label className="text-sm text-gray-500">Date of Expiration</label>
-        <input
-          type="date"
-          placeholder="Date of Expiration"
-          className="flex-grow text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
-        />
-      </div>
+  <div className="flex items-center justify-between">
+    <label className="text-sm text-gray-500">Date of Expiration</label>
+    <input
+      type="date"
+      placeholder="Date of Expiration"
+      className="w-10/12 text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
+    />
+  </div>
 
-      <div className="flex items-center space-x-3">
-        <label className="text-sm text-gray-500">Supplier Name</label>
-        <input
-          type="text"
-          placeholder="Supplier Name"
-          className="flex-grow text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
-        />
-      </div>
+  <div className="flex items-center justify-between">
+    <label className="text-sm text-gray-500">Supplier Name</label>
+    <input
+      type="text"
+      placeholder="Supplier Name"
+      className="w-10/12 text-sm py-0.5 px-6 text-gray-500 ring-1 ring-gray-300 focus:ring-1 focus:ring-gray-300 focus:outline-none rounded-md placeholder:mr-5"
+    />
+  </div>
 
-      <div className="mt-6">
-        <button className="bg-black text-white text-xs font-semibold px-3 py-1.5 rounded-md">
-          Add Inventory
-        </button>
-      </div>
-    </div>
+  <div className="mt-6">
+    <button className="bg-black text-white text-xs font-semibold px-3 py-1.5 rounded-md">
+      Add Inventory
+    </button>
+  </div>
+</div>
+
   </form>
 </div>
 

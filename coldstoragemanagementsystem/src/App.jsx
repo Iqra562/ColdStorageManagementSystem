fd1d9ccc932523cd34974f33ae35f5ac790491c0
Muhammad Qasim khan <164278955+mqkhan125@users.noreply.github.com>
2024-09-23 13:32:00 +0500

    Update App.jsx

--- coldstoragemanagementsystem/src/App.jsx
+++ coldstoragemanagementsystem/src/App.jsx
@@ -12,9 +12,16 @@ import AddInventory from './pages/Inventory/AddInventory.jsx'
 import EditInventory from './pages/Inventory/EditInventory.jsx'
 import DeleteConfirmationModal from './components/DeleteConfirmationModal.jsx'
 import Login from './pages/login.jsx'
-function App() {
+import Suplier from './pages/Manager Suplier/Suplier.jsx';
+import AddSuplier from './pages/Manager Suplier/AddSuplier.jsx';
+import EditSuplier from './pages/Manager Suplier/EditSuplier.jsx';
+import Storage from './pages/Manage Storage/Storage.jsx'
+import AddStorage from './pages/Manage Storage/AddStorage.jsx'
+import EditStorage from './pages/Manage Storage/EditStorage.jsx'
 
+function App() {
   return (
+    
     <>
 <BrowserRouter>
 <Routes>
@@ -23,13 +30,20 @@ function App() {
     <Route path='/inventory' element={<Inventory/>}/>
     <Route path='/addinventory' element={<AddInventory/>}/>
     <Route path='/editinventory' element={<EditInventory/>}/>
+    <Route path='/suplier' element={<Suplier/>}/>
+    <Route path='/addSuplier' element={<AddSuplier/>}/>
+    <Route path='/editSuplier' element={<EditSuplier/>}/>
+    <Route path='/storage' element={<Storage/>}/>
+    <Route path='/addStorage' element={<AddStorage/>}/>
+    <Route path='/editStorage' element={<EditStorage/>}/>
+   
   </Route>
     <Route path='/login' element={<Login/>}/>
 </Routes>
 </BrowserRouter>
-   
+
     </>
   )
 }
 
-export default App
+export default App;
